218aee0f811faed229e68bdd81c61486decea879
Adam Poit <metalkeys404@gmail.com>
2015-07-02 00:38:39 -0700

    Remove files.

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),
+  del = require('del'),
+  install = require('gulp-install');
+
+gulp.task('clean', function (cb) {
+  del(['static'], cb);
+});
+
+gulp.task('install', function () {
+  return gulp.src('./package.json')
+    .pipe(install());
+});
+
+gulp.task('build', ['clean'], function () {
+  return gulp.src('content/**/*')
+    .pipe(gulp.dest('static'));
+});
+
+gulp.task('default', ['install', 'build']);
